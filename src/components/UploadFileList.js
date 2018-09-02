7595525355f75a08ad42a2c9d06c4e374811edca
casa <diogo.alves@gmail.com>
2018-09-02 16:53:11 -0300

    feat: ads a file listing component

---
+++ src/components/UploadFileList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { prettyBytes } from '../utils';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  }
+});
+
+function FolderList(props) {
+  const { classes, files } = props;
+  if (!files) return null;
+  console.log(files);
+  return (
+    <div className={classes.root}>
+      <List>
+        {Array.from(files).map(e => (
+          <ListItem key={e.name} dense button>
+            <ListItemText
+              primary={e.name}
+              secondary={`${prettyBytes(e.size)} ${e.type}`}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default withStyles(styles)(FolderList);
