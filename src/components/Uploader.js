696f1b8238436384355cc5e0f9b76b74c9883f54
casa <diogo.alves@gmail.com>
2018-09-02 16:55:09 -0300

    feat: improves upload component ui

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -7,63 +7,226 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import AddIcon from '@material-ui/icons/Add';
 import Button from '@material-ui/core/Button';
-import Dropzone from 'react-dropzone';
+import { prettyBytes } from '../utils';
+import UploadFileList from './UploadFileList';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import ShareIcon from '@material-ui/icons/Share';
+import copy from 'copy-to-clipboard';
 
-// TODO learn how to create a file list
 // TODO send email using emailjs.com
-// TODO style hang card in a similar place as wetransfer
-// TODO add seed status on page
 // TODO try using zip.js to create a protected content
-// TODO discover a way
+// TODO try on update insteade of a setInterval
+// TODO when the file is big add some loading
 const client = new WebTorrent();
 
 const styles = theme => ({
+  root: {
+    paddingTop: '50px',
+    paddingLeft: '50px'
+  },
   card: {
-    maxWidth: 300
+    width: '100%',
+    maxWidth: 280,
+    paddingBottom: '20px'
   },
   actions: {
-    display: 'flex'
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  input: {
+    display: 'none'
+  },
+  fabProgress: {
+    position: 'relative',
+    top: -20,
+    zIndex: 1
+  },
+  textProgress: {
+    position: 'relative',
+    top: 77,
+    zIndex: 1
   }
 });
 
 class Uploader extends Component {
+  state = {
+    files: null,
+    infoHash: null,
+    magnet: null,
+    torrentFile: null,
+    torrentFileBlobURL: null,
+    uploaded: 0,
+    uploadSpeed: 0,
+    ratio: 0,
+    numPeers: 0,
+    graph: {
+      nodes: [{ id: 'me', name: 'me' }],
+      links: []
+    },
+    interval: null,
+    progress: 0,
+    size: 0
+  };
+
   seed = files => {
-    client.seed(files, torrent => {
+    client.seed(this.state.files, torrent => {
+      this.setState({
+        infoHash: torrent.infoHash,
+        magnet: torrent.magnetURI,
+        torrentFile: torrent.torrentFile,
+        torrentFileBlobURL: torrent.torrentFileBlobURL
+      });
       console.log('Client is seeding ' + torrent.magnetURI);
+
+      var interval = setInterval(() => {
+        this.setState({
+          numPeers:
+            torrent.numPeers + (torrent.numPeers === 1 ? ' peer' : ' peers'),
+          uploadSpeed: prettyBytes(torrent.uploadSpeed) + '/s',
+          uploaded: prettyBytes(torrent.uploaded),
+          ratio: torrent.ratio,
+          progress: Math.round((torrent.uploaded / this.state.size) * 100)
+        });
+      }, 1000);
+      this.setState({
+        interval
+      });
+
+      torrent.on('wire', (wire, addr) => {
+        this.setState(state => ({
+          graph: {
+            nodes: state.graph.nodes.concat({ id: addr, name: addr }),
+            links: state.graph.links.concat({ source: 'me', target: addr })
+          }
+        }));
+      });
     });
+  };
+
+  handleChange = event => {
+    clearInterval(this.state.interval);
+    const files = event.target.files;
+    const size = Array.from(files).reduce((acc, cur) => acc + cur.size, 0);
+
+    this.setState({
+      files: files,
+      infoHash: null,
+      magnet: null,
+      torrentFile: null,
+      torrentFileBlobURL: null,
+      uploaded: 0,
+      uploadSpeed: 0,
+      ratio: 0,
+      numPeers: 0,
+      graph: {
+        nodes: [{ id: 'me', name: 'me' }],
+        links: []
+      },
+      interval: null,
+      progress: 0,
+      size
+    });
+  };
+
+  copyMagnetToClipboard = () => {
+    copy(this.state.magnet);
+  };
+
+  downloadTorrent = () => {
+    window.location.assign(this.state.torrentFileBlobURL);
   };
 
   render() {
     const { classes } = this.props;
-
+    const { files, infoHash, numPeers, uploadSpeed, progress } = this.state;
     return (
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Button
-              variant="fab"
-              color="secondary"
-              aria-label="Add"
-              className={classes.button}
-            >
-              <AddIcon />
-            </Button>
-          }
-          title="Add your files"
-        />
-        <CardContent>
-          <Dropzone onDrop={this.seed} />
-        </CardContent>
-        <CardActions className={classes.actions} disableActionSpacing>
-          <Button
-            variant="extendedFab"
-            color="secondary"
-            className={classes.button}
-          >
-            Transfer
-          </Button>
-        </CardActions>
-      </Card>
+      <div className={classes.root}>
+        <Card className={classes.card}>
+          {!files && (
+            <CardHeader
+              avatar={
+                <React.Fragment>
+                  <input
+                    className={classes.input}
+                    id="flat-button-file"
+                    multiple
+                    type="file"
+                    onChange={this.handleChange}
+                  />
+                  <label htmlFor="flat-button-file">
+                    <Button
+                      variant="fab"
+                      color="secondary"
+                      aria-label="Add"
+                      className={classes.button}
+                      component="span"
+                    >
+                      <AddIcon />
+                    </Button>
+                  </label>
+                </React.Fragment>
+              }
+              title="Add your files"
+            />
+          )}
+
+          <CardContent align="center">
+            {!infoHash && <UploadFileList files={files} />}
+
+            {infoHash && (
+              <React.Fragment>
+                <Typography className={classes.textProgress} variant="display2">
+                  {progress}
+                </Typography>
+                <CircularProgress
+                  className={classes.fabProgress}
+                  size={144}
+                  color="secondary"
+                  variant="determinate"
+                  value={progress}
+                />
+                <Typography
+                >{`Seeding ↑${uploadSpeed} to ${numPeers}`}</Typography>
+                <Typography variant="caption">
+                  Send someone this link to download. This link will work as
+                  long as this page is open.
+                </Typography>
+              </React.Fragment>
+            )}
+          </CardContent>
+          <CardActions className={classes.actions} disableActionSpacing>
+            {!infoHash && (
+              <Button
+                variant="extendedFab"
+                color="secondary"
+                className={classes.button}
+                disabled={!files}
+                onClick={this.seed}
+              >
+                Transfer
+              </Button>
+            )}
+            {infoHash && (
+              <React.Fragment>
+                <IconButton onClick={this.downloadTorrent}>
+                  <GetAppIcon />
+                </IconButton>
+                <IconButton onClick={this.copyMagnetToClipboard}>
+                  <FlashOnIcon />
+                </IconButton>
+                <IconButton>
+                  <ShareIcon onClick={() => console.log(this.state)} />
+                </IconButton>
+              </React.Fragment>
+            )}
+          </CardActions>
+        </Card>
+      </div>
     );
   }
 }
